e929da2ecfd0bec28f1b75aeb43a4976abf3e4b1
Kingsley Udenwu <kingzpacking@gmail.com>
2020-11-26 22:20:20 +0100

    Add validate scope middleware to the route middleware

--- app/Http/Middleware/ValidateScope.php
+++ app/Http/Middleware/ValidateScope.php
@@ -2,20 +2,32 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ApiResponse;
 use Closure;
 use Illuminate\Http\Request;
 
 class ValidateScope
 {
+    use ApiResponse;
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @param mixed ...$scopes
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next, ...$scopes)
     {
-        return $next($request);
+        if (! $request->user() || ! $request->user()->token()) {
+            return $this->badRequestAlert('Invalid token credentials');
+        }
+        foreach ($scopes as $scope) {
+            if ($request->user()->tokenCan($scope)) {
+                return $next($request);
+            }
+        }
+
+        return $this->forbiddenRequestAlert('Not Authorized.');
     }
 }
