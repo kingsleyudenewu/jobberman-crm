de29d0f10a438599cd2c82335f17bf825b7b7b9a
Kingsley Udenwu <kingzpacking@gmail.com>
2020-11-26 14:36:27 +0100

    Add validation for login feature test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -27,4 +27,19 @@ class AuthTest extends TestCase
         $this->json('POST','/api/v1/auth/login', $payload, $this->headers)->assertStatus(200);
         $this->assertAuthenticated();
     }
+
+    public function testMustEnterEmail()
+    {
+        $this->withoutExceptionHandling();
+        $payload = [
+            'email' => '',
+            'password' => 'password',
+        ];
+        $this->json('POST','/api/v1/auth/login', $payload, $this->headers)
+            ->assertStatus(422)
+            ->assertJsonFragment([
+                'statusCode' => 422,
+                'message' => 'Whoops. Validation failed.'
+            ]);
+    }
 }
