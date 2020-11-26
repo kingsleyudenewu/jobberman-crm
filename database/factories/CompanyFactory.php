b5ab44e9d165cc7ac4df4739f99c98ce1425593a
Kingsley Udenwu <kingzpacking@gmail.com>
2020-11-26 02:29:41 +0100

    Add unit test and create relationship between company and employee

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -22,7 +22,12 @@ class CompanyFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'url' => $this->faker->url,
+            'logo' => 'company/'.$this->faker->image(storage_path('app/public/company'), 500,
+                    500, null, false),
         ];
     }
 }
---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
