9b85dd32bdfee8a957de2dfb78729ec48e2fdc71
Kingsley Udenwu <kingzpacking@gmail.com>
2020-11-27 13:58:02 +0100

    Add actions and mutations for auth route on vuejs

--- resources/js/store/actions.js
+++ resources/js/store/actions.js
@@ -1,3 +1,35 @@
-export default {
+import axios from 'axios';
 
+export default {
+    loginAction: async ({commit}, {email, password}) => {
+        try{
+            const response = await axios({
+                'method': 'post',
+                'url': '/api/v1/auth/login',
+                data: {
+                    email: email,
+                    password: password
+                }
+            });
+
+            if (response.message === 'success') {
+                commit('authSuccess', response.data.access_token )
+            }
+        }
+        catch (error){
+            commit('authError')
+        }
+    },
+
+    logoutAction: async ({commit}, token) => {
+        try {
+            const response = await axios({
+                'method': 'post',
+                'url': '/api/v1/logout',
+            });
+        }
+        catch (error) {
+            commit('authError')
+        }
+    }
 }
--- resources/js/store/mutations.js
+++ resources/js/store/mutations.js
@@ -1,3 +1,10 @@
 export default {
-
+    authSuccess: (state, token) => {
+        state.isLoggedIn = true;
+        state.token = token;
+    },
+    authError: (state) => {
+        state.isLoggedIn = false;
+        state.token = null;
+    }
 }
